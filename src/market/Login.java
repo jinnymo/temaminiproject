12b3a1c31e149fb59d14f3d5b5d1703c44b9d5f9
bbangEor <eor1402@naver.com>
2024-06-18 17:08:44 +0900

    .

--- src/market/Login.java
+++ src/market/Login.java
@@ -1,45 +1,86 @@
 package market;
 
+import java.awt.Color;import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Login extends JPanel {
 
-	
 	private JTextField idField;
 	private JTextField pwdField;
 	private JButton btnLogin;
 	private JButton btnJoin;
-
+	private Register register;
+	private MainFrame context;
 	// 생성자
-	public Login() {
-		
-		idField = new JTextField("아이디 입력.");
-		pwdField = new JTextField("패스워드 입력.");
-		btnLogin = new JButton();
-		
-		
-		
+	public Login(MainFrame context) {
+		this.context = context;
 		initData();
 		setInitLayout();
-		addEventListener();
-		
-	}
+		addActionListener();
 
-	private void addEventListener() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void setInitLayout() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	private void initData() {
-		// TODO Auto-generated method stub
+
+		idField = new JTextField("아이디 입력.");
+		pwdField = new JTextField("패스워드 입력.");
+		btnLogin  = new JButton ("LOGIN");
+		btnJoin = new JButton();
+
+	}
+
+	private void setInitLayout() {
+		setLayout(null);
+		setSize(Resource.PANEL_FULL_SIZE_X, Resource.PANEL_FULL_SIZE_Y);
+		setLocation(0, 0);
+		setBackground(Color.green);
+		setVisible(true);
+		
+		add(idField);
+		add(pwdField);
+		add(btnLogin);
+		add(btnJoin);
+		
+		idField.setSize(Resource.LOGIN_COMP_X,Resource.LOGIN_COMP_Y);
+		pwdField.setSize(Resource.LOGIN_COMP_X,Resource.LOGIN_COMP_Y);
+		btnLogin.setSize(Resource.LOGIN_COMP_X - 50,Resource.LOGIN_COMP_Y );
+		btnJoin.setSize(Resource.LOGIN_COMP_X - 50,Resource.LOGIN_COMP_Y);
+		
+		idField.setLocation(100,100);
+		pwdField.setLocation(100,140);
+		btnLogin.setLocation(125,180);
+		btnJoin.setLocation(125,220);
+
+	}
+
+	private void addActionListener() {
+	// 로그인 버튼 -> 메인페이지 or RETRY
+	btnLogin.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			
+			super.mousePressed(e);
+		}
+		
+	});
+	// 회원가입 버튼 -> 회원가입 상세
+	btnJoin.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			 register = new Register(context);
+			 setVisible(false);
+			
+		}
+		
+	});
+	
 		
 	}
-	
+
 }
--- src/market/MainFrame.java
+++ src/market/MainFrame.java
@@ -1,21 +1,28 @@
 package market;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
+import lombok.Data;
+
 public class MainFrame extends JFrame{
- 
+	Login a;
+
 	public MainFrame() {
+	a = new Login(this);
+	
 	initData();
 	setInitLayout();
 		
 	}
-
+	
 	private void initData() {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(Resource.FRAMESIZE_X,Resource.FRAMESIZE_Y);
-		setResizable(false);
-
+		//setResizable(false);
+		setBackground(Color.black);
 		
 	}
 
@@ -24,6 +31,9 @@ public class MainFrame extends JFrame{
 		setLayout(null); //좌표기준으로만
 		setLocation(0,0);
 		setVisible(true);
+		
+		add(a);
+
 	}
 	
 	public static void main(String[] args) {
