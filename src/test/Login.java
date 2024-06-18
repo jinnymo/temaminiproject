7e21fb3209cb025a9a88331dc287e989ae849939
bbangEor <eor1402@naver.com>
2024-06-18 15:17:52 +0900

    .

--- src/test/Login.java
+++ src/test/Login.java
@@ -1,7 +1,42 @@
 package test;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Login extends JPanel {
 
+	private JTextField idField;
+	private JTextField pwdField;
+	private JButton submit;
+
+	// 생성자
+	public Login() {
+		
+		idField = new JTextField("아이디 입력.");
+		pwdField = new JTextField("패스워드 입력.");
+		submit = new JButton();
+		
+		
+		initData();
+		setInitLayout();
+		addEventListener();
+		
+	}
+
+	private void addEventListener() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setInitLayout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
