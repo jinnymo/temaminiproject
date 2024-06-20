017750a0b3bc0834f294b8217b27f045d878bd91
jinnymo <ehddbs1105@gmail.com>
2024-06-20 09:13:49 +0900

    .

--- src/market/MainFrame.java
+++ src/market/MainFrame.java
@@ -5,12 +5,13 @@ import java.awt.Color;
 import javax.swing.JFrame;
 
 import lombok.Data;
-
+import lombok.Getter;
+@Getter
 public class MainFrame extends JFrame {
 
 	public Login loginPanel;
 	private PanelAdapter panelAdapter;
-
+	private UserDTO myUserDTO;
 	public MainFrame() {
 		loginPanel = new Login(this);
 
@@ -35,7 +36,8 @@ public class MainFrame extends JFrame {
 	}
 
 	public void startMainSystem(UserDTO MyUserDTO) {
-		panelAdapter = new PanelAdapter();
+		this.myUserDTO = MyUserDTO;
+		panelAdapter = new PanelAdapter(this);
 		panelAdapter.setSize(Resource.FRAMESIZE_X, Resource.FRAMESIZE_Y); // 패널 크기 설정
 		panelAdapter.setLocation(0, 0); // 패널 위치 설정
 
