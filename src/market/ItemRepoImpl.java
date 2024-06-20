8e0cf66a9bd0605db01c01df0b0ec9f2a32f4cce
bbangEor <eor1402@naver.com>
2024-06-20 14:24:14 +0900

    .

--- src/market/ItemRepoImpl.java
+++ src/market/ItemRepoImpl.java
@@ -11,11 +11,9 @@ public class ItemRepoImpl implements ItemRepo {
 	public int addItem(String productName, String price, String state, String content, int myUserNum, int categoryId)
 			throws SQLException {
 		int rowCount = 0;
-<<<<<<< HEAD
+
 		String query = " INSERT INTO item (product_name, price, state, date, product_info, user_num, category_id) "
-=======
-		String query = " INSERT INTO item (product_name, price, state, date, product_info, user_num, category_ID)\r\n"
->>>>>>> MHv5
+
 				+ "VALUES(?, ?, ?, now(), ?, ?, ?) ";
 		try (Connection conn = DBConnectionManager.getInstance().getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(query)) {
